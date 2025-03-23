912de9694ed3ba57f3d076e65b105cf18979918b
Srinjoy <srinjoypramanik2004@gmail.com>
2025-03-23 23:08:00 +0530

    vid 1 30 mins basic setup done!

---
+++ src/controllers/home-controller.js
@@ -0,0 +1,21 @@
+app.get('/api/v1/blogs',(req,res)=>
+
+    {
+     
+    })
+    app.post('/api/v1/blogs',(req,res)=>
+    
+    {
+     
+    })
+    app.put('/api/v1/blogs',(req,res)=>
+    
+    {
+     
+    })
+    app.get('/api/v1/blogs',(req,res)=>
+    
+    {
+     
+    })
+    
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express =require ('express')
+
+const {PORT}=require('./config')
+
+const app=express();
+
+
+
+app.listen(PORT,()=>
+{
+    console.log(`successfully started the server on PORT : ${PORT}`)
+})
